452d1af77988f4730baf48e0eccf7d3e1f112b93
dead-beef <dead-beef@users.noreply.github.com>
2017-10-13 00:55:16 +0000

    fix package data

--- setup.py
+++ setup.py
@@ -1,35 +1,50 @@
+#!/usr/bin/env python3
+
+import os
 from unittest import TestLoader
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def tests():
     return TestLoader().discover('test')
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(BASE_DIR, 'README.rst')) as fp:
+        README = fp.read()
+except FileNotFoundError:
+    README = ''
+
 setup(name='markovchain',
-      version='0.1',
+      version='0.1.0',
       description='Markov chain generator',
-      long_description='',
+      long_description=README,
       classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
-          'License :: OSI Approved :: MIT License'
+          'Topic :: Multimedia :: Graphics',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Text Processing',
+          'Topic :: Utilities'
       ],
       keywords='markov chain generator',
       url='https://github.com/dead-beef/markovchain',
       author='dead-beef',
       author_email='contact@dead-beef.tk',
       license='MIT',
-      packages=['markovchain'],
+      packages=find_packages(include=('markovchain*',)),
       entry_points={
           'console_scripts': ['markovchain=markovchain.cli:main'],
       },
-      package_data={
-          '': ['README.md', 'LICENSE'],
-      },
-      #test_loader='unittest:TestLoader',
       test_suite='setup.tests',
       install_requires=['tqdm', 'ijson'],
       extras_require={
           'image': ['pillow']
       },
+      python_requires='>=3',
       include_package_data=True,
       zip_safe=False)
