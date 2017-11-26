53728dbeb2aa8e0d69c25017967c50cd46577ddc
dead-beef <dead-beef@users.noreply.github.com>
2017-11-26 11:13:13 +0000

    fix build script

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-rm -rf build/* dist/* docs/* docsrc/source/*
-sed -r 's/`([^`]+)`/\1/g' README.md \
-    | pandoc -f markdown_github -t rst >README.rst
-make -C docsrc apidoc html
-python3 setup.py sdist
-python3 setup.py bdist_wheel
+rm -rfv build/* dist/* docs/* docsrc/source/* \
+    && (sed -r 's/`([^`]+)`/\1/g' README.md \
+        | pandoc -f markdown_github -t rst >README.rst) \
+    && make -C docsrc apidoc html \
+    && python3 setup.py sdist bdist_wheel
