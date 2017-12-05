7c3c0b59a763e12cd82ba2f5e784b3f446d57f1f
dead-beef <dead-beef@users.noreply.github.com>
2017-12-05 07:23:40 +0000

    update scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -d env ]; then
+    . env/bin/activate
+fi
+
 rm -rfv build/* dist/* docs/* docsrc/source/* \
     && make -C docsrc apidoc html \
     && python3 setup.py sdist bdist_wheel
